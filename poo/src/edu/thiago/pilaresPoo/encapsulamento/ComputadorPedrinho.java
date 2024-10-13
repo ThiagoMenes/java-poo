0f35320f3029aea14082f1a37b5b3beeeb2109ec
Thiago <thiagoesmael19@gmail.com>
2024-10-13 16:35:57 -0300

    Feat: add Aprofundamento encapsulamento

---
+++ poo/src/edu/thiago/pilaresPoo/encapsulamento/ComputadorPedrinho.java
@@ -0,0 +1,11 @@
+package edu.thiago.pilaresPoo.encapsulamento;
+
+public class ComputadorPedrinho {
+    public static void main(String[] args) {
+        MSNMessenger msn = new MSNMessenger();
+
+        msn.enviarMensagem();
+
+        msn.receberMensagem();
+    }
+}
--- poo/src/edu/thiago/pilaresPoo/encapsulamento/MSNMessenger.java
+++ poo/src/edu/thiago/pilaresPoo/encapsulamento/MSNMessenger.java
@@ -1,19 +1,21 @@
-package edu.thiago.pilaresPoo;
+package edu.thiago.pilaresPoo.encapsulamento;
 
 public class MSNMessenger {
     public void enviarMensagem(){
+        validarConectadoInternet();
         System.out.println("Enviando Mensagem");
+        salvarHistoricoMensagem();
     }
 
     public void receberMensagem(){
         System.out.println("Recebendo Mensagem");
     }
 
-    public void validarConectadoInternet(){
+    private void validarConectadoInternet(){
         System.out.println("Validando se está conectado na Internet");
     }
 
-    public void salvarHistoricoMensagem(){
+    private void salvarHistoricoMensagem(){
         System.out.println("Salvando o histórico da mensagem");
     }
 }
